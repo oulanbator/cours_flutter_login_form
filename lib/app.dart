e269560c7c4a72ecb5af8c94e7249a2d23c1c5e1
Victor Matheron <victor.matheron@gmail.com>
2024-03-14 11:13:37 +0100

    Base structure of the app

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:cours_flutter_login_form/screens/login_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:cours_flutter_login_form/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
