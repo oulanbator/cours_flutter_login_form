da6ec07750ede336e84b84580631329445f19821
Victor Matheron <victor.matheron@gmail.com>
2024-03-14 11:32:25 +0100

    Implements login form

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -8,11 +8,55 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Formulaire de login"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: usernameController,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), labelText: "Email"),
+            ),
+            SizedBox(height: 12),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), labelText: "Password"),
+            ),
+            SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: () {
+                var username = usernameController.text;
+                var password = passwordController.text;
+
+                if (username == "admin" && password == "password") {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      duration: Duration(seconds: 0),
+                      content: Text("Success"),
+                    ),
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      duration: Duration(seconds: 0),
+                      content: Text("Error"),
+                    ),
+                  );
+                }
+              },
+              child: Text("Login"),
+            )
+          ],
+        ),
       ),
     );
   }
